3bbc41f75c6a60f601bfa65e512da9cd5afb8d2c
thokalanehasri <93606255+thokalanehasri@users.noreply.github.com>
2022-09-22 15:10:54 +0530

    Add files via upload

---
+++ experiment 1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+df = pd.read_csv(r'file:///home/cse/Desktop/prog2.csv')
+
+
+# In[3]:
+
+
+print(df)
+
+
+# In[ ]:
+
+
+
+
